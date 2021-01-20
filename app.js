5770dd4ea2304c622e51aa7caffd57fc6e8f00e9
wrogoz <wrogoz@gmail.com>
2021-01-20 21:04:26 +0100

    login route in progress

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ require("dotenv").config();
 const db = require("./db/db_config");
 
 const auth = require("./middlewares/auth");
-const {registerUser,showAllUsers} = require("./controllers/usersController");
+const {registerUser,showAllUsers,loginUser} = require("./controllers/usersController");
 const app = express();
 app.use(express.json());
 
 app.get("/", auth, showAllUsers);
 app.post("/register", registerUser);
-
+app.post('/login', loginUser);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,57 +1,83 @@
 const db = require("../db/db_config");
-var bcrypt = require('bcryptjs');
+var bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
 
-
-const showAllUsers = (req,res)=>{
-    
+const showAllUsers = (req, res) => {
   const sql = "SELECT * FROM users ";
   db.query(sql, (err, result) => {
     if (err) throw err;
-    if(result.length===0){
-
-        res.send('no user found')
-    }else{
-
-        res.send(result);
+    if (result.length === 0) {
+      res.send("no user found");
+    } else {
+      res.send(result);
     }
-
   });
-}
+};
 
-const registerUser = async(req,res)=>{
-    try {
-        const {email,password}= req.body;
-        var hashedPassword =await bcrypt.hash(password, 8);
+const registerUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    var hashedPassword = await bcrypt.hash(password, 8);
 
-        const sql = `INSERT INTO USERS SET?`;
-        const user_data={
-            email,
-            user_password:hashedPassword
+    const sql = `INSERT INTO USERS SET?`;
+    const user_data = {
+      email,
+      user_password: hashedPassword,
+    };
+
+    db.query(sql, user_data, (err, result) => {
+      if (err) throw err;
+
+      const user_data_query = "INSERT INTO users_data SET?";
+      const user_id = { user_id: result.insertId };
+
+      db.query(user_data_query, user_id, (err, user_data_res) => {
+        if (err) throw err;
+      });
+      const address_query = "INSERT INTO addresses SET?";
+      db.query(address_query, user_id, (err, addresses_res) => {
+        if (err) throw err;
+      });
+      console.log(result.insertId);
+      const token = jwt.sign({ id: result.insertId }, process.env.JWT_SECRET);
+      res.send(token);
+    });
+  } catch (error) {
+    res.status(500).send(`server error: ${error}`);
+  }
+};
+
+const loginUser = (req, res) => {
+  try {
+    const sql = `SELECT * FROM users WHERE email = ? `;
+    const user = req.body.email;
+
+    const query = db.query(sql, user, async (err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+        res.send("user not found");
+      } else {
+        console.log(result);
+        const checkPassword = await bcrypt
+          .compare(req.body.password, result[0].user_password)
+          .then((res) => {
+            return res;
+          });
+
+        if (!checkPassword) {
+          res.send("invalid password");
+        } else {
+          const token = jwt.sign(
+            { id: result[0].id, email: result[0].email },
+            process.env.JWT_SECRET
+          );
+          res.send(token);
         }
+      }
+    });
+  } catch (error) {
+    res.status(500).send(`server error:${error}`);
+  }
+};
 
-        db.query(sql,user_data,(err,result)=>{
-            if(err)throw err;
-            console.log(result.insertId)
-
-            const user_data_query='INSERT INTO users_data SET?'
-            const user_id={user_id:result.insertId}
-
-            db.query(user_data_query,user_id,(err,user_data_res)=>{
-                if(err)throw err;
-
-            })
-            const address_query='INSERT INTO addresses SET?'
-            db.query(address_query,user_id,(err,addresses_res)=>{
-                if(err)throw err;
-
-            })
-            res.send(result)
-        })
-
-            } catch (error) {
-                res.status(500).send(`server error: ${error}`)
-            }
-
-}
-
-module.exports={registerUser,showAllUsers};
+module.exports = { registerUser, showAllUsers, loginUser };
