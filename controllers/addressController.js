89eee2e4f663273c8e4f00d72ee7e04499a76713
wrogoz <wrogoz@gmail.com>
2021-01-23 23:31:58 +0100

    user info table and addresses table update function cmpltd

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -1,31 +1,40 @@
 const db = require("../db/db_config");
 
+const getAddress = async (req, res) => {
+  try {
+    const sql = `SELECT * FROM addresses
+                    WHERE user_id=?`;
+    const { id } = req.body.user;
+    console.log(id);
 
-const getAddress = async(req, res) => {
-    try {
-        const sql = `SELECT * FROM addresses
-                    WHERE user_id=?`
-        const {id}=req.body.user;
-        console.log(id)
+    db.query(sql, id, (err, result) => {
+      if (err) throw err;
+      res.send(result);
+    });
+  } catch (error) {
+    res.status(500).send({ "server error": error });
+  }
+};
 
-        db.query(sql,id,(err,result)=>{
-            if(err)throw err;
-            res.send(result)
-        })
+const updateAddress = async (req, res) => {
+  try {
+    const user_id = req.body.user.id;
+    const params = req.body;
+    delete params["user"];
 
-    } catch (error) {
-        res.status(500).send({'server error':error})
+    if (Object.keys(params).length === 0) {
+      res.status(400).send("no data provided");
+    } else {
+      const sql = `UPDATE addresses
+                    SET ? WHERE user_id=?`;
+      db.query(sql, [params, user_id], (err, result) => {
+        if (err) throw err;
+        res.status(200).send({ status: "success" });
+      });
     }
+  } catch (error) {
+    res.status(500).send({ "server error": error });
+  }
+};
 
-}
-
-const updateAddress = async(req, res) => {
-    try {
-        res.status(200).send('update address')
-    } catch (error) {
-        res.status(500).send({'server error':error})
-    }
-
-}
-
-module.exports={getAddress,updateAddress}
+module.exports = { getAddress, updateAddress };
--- controllers/usersInfoController.js
+++ controllers/usersInfoController.js
@@ -1,31 +1,40 @@
 const db = require("../db/db_config");
 
+const getUserInfo = async (req, res) => {
+  try {
+    const sql = `SELECT * FROM users_info
+                    WHERE user_id=?`;
+    const { id } = req.body.user;
+    console.log(id);
 
-const getUserInfo =async(req,res)=>{
-    try {
-        const sql = `SELECT * FROM users_info
-                    WHERE user_id=?`
-        const {id}=req.body.user;
-        console.log(id)
+    db.query(sql, id, (err, result) => {
+      if (err) throw err;
+      res.status(200).send(result);
+    });
+  } catch (error) {
+    res.status(500).send({ "server error": error });
+  }
+};
+const updateUserInfo = async (req, res) => {
+  try {
+    user_id = req.body.user.id;
+    let params = req.body;
+    delete params["user"];
 
-        db.query(sql,id,(err,result)=>{
-            if(err)throw err;
-            res.send(result)
-        })
+    if (Object.keys(params).length === 0) {
+      res.send("no data provided");
+    } else {
+      const sql = `UPDATE users_info
+            SET ? WHERE user_id=?`;
 
-    } catch (error) {
-        res.status(500).send({'server error':error})
+      db.query(sql, [params, user_id], (err, result) => {
+        if (err) throw err;
+        res.status(200).send({ status: "success" });
+      });
     }
-}
-const updateUserInfo =async(req,res)=>{
-    try {
-        res.status(299).send('updateUserInfo')
-    } catch (error) {
-        res.status(500).send({'server error':error})
-    }
-}
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
 
-
-module.exports = {getUserInfo,updateUserInfo};
-
-
+module.exports = { getUserInfo, updateUserInfo };
